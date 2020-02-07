bbbd9d59396c1291623d7014a672627f667c2e2f
ming0520 <mingblog0520@gmail.com>
2020-02-07 10:13:55 +0800

    Update BST.cpp
    -Add command
    -Modify Insert
    -Add remove, minvalueNode,inorder,grapth,search2,inorderaux,graphAux

--- BST.cpp
+++ BST.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 class BST{
-    
+    public:
     class Node{
         public:
         int Data;
@@ -17,18 +17,30 @@ class BST{
         }
     };
 
-    public:
-    Node *Root;
-
     
+    Node *Root;
+//============================================================================
+// Purpose       : Construct a BST object.
+// Precondition  : None.
+// Postcondition : An empty BST has been constructed.
+//============================================================================
     BST(void){
         Root = 0;
     }
-
+//============================================================================
+// Purpose       : Check if BST is empty.
+// Precondition  : None.
+// Postcondition : Returns true if BST is empty and false otherwise.
+//============================================================================
     bool Empty(void){
         return (Root == 0);
     }
 
+//============================================================================
+// Purpose       : Search the BST for item.
+// Precondition  : None.
+// Postcondition : Returns true if item found, and false otherwise.
+//============================================================================
     bool Search(int item){
         Node *locptr = Root;
         bool found = false;
@@ -48,15 +60,20 @@ class BST{
         }
         return found;
     }
-
+//============================================================================
+// Purpose       : Insert item into BST.
+// Precondition  : None.
+// Postcondition : BST has been modified with item inserted at proper 
+//                 position to maintain BST property. 
+//============================================================================
     void Insert(int item){
-        Node *locptr, *parrent;
+        Node *locptr, *parent;
         bool found = false;
         locptr = Root;
-        parrent = 0;
+        parent = 0;
         
         while(!found || locptr != 0){
-            parrent = locptr;
+            parent = locptr;
             if(item < locptr->Data){
                 locptr = locptr->Left;
             }
@@ -70,20 +87,162 @@ class BST{
 
         if(!found){
             locptr = new Node(item);
-            if(parrent == 0){
+            if(parent == 0){
                 Root =locptr;
             }
-            else if(item < parrent->Data){
-                parrent->Left = locptr;
+            else if(item < parent->Data){
+                parent->Left = locptr;
             }
             else{
-                parrent->Right = locptr;
+                parent->Right = locptr;
             }
         }
         else{
             cout << "Item already in the list" << endl;
         }
-    }   
+    } 
+//============================================================================
+// Purpose       : Find Min value in non empty BST
+// Precondition  : None
+// Postcondition : Return address of min value in BST
+//============================================================================
+    Node *minValueNode(){
+        Node *current = Root;
+        while(current && current->Left != 0){
+            current = current->Left;
+        }
+
+        return current;
+    }    
+//============================================================================
+// Purpose       : Remove item from BST.
+// Precondition  : None.
+// Postcondition : BST has been modified with item removed (if present);
+//                 BST property is maintained.
+// Note          : remove uses auxiliary function search2() to locate the node
+//                 containing item and its parent.
+//============================================================================
+/* Scenario for delete
+    1. Empty tree
+    2. Node to be deleted is leaf
+    3. Node to be deleted has only one child
+    3. Node to be deleted has only two child
+*/    
+    void Remove(int item){
+        bool found;
+        Node *x, *parent;
+        search2(item,found,x,parent);
+
+        if(!found){
+            cout << "Item not in the BST\n";
+            return;
+        }
+
+        if(x->Left != 0 && x->Right != 0){ //Node has two child
+            Node *xSucc = x->Right;
+            parent = x;
+            while(xSucc->Left != 0){ //decend left = find smallest
+                parent = xSucc;
+                xSucc = xSucc->Left;
+            }
+
+            //move contens of xSucc to x and change xSucc
+            //to point of successor, which will be removed.
+            x->Data = xSucc->Data;
+            x = xSucc;
+        } // end if node has two child
+
+        // Now proceed with case where node has 0 or 2 child
+        Node *subtree = x->Left;
+        if(subtree == 0){
+            subtree = x->Right;
+        }
+        if(parent == 0){
+            Root = subtree;
+        }
+
+    }
+
+
+
+//============================================================================
+// Purpose       : Inorder traversal of BST.
+// Precondition  : ostream out is open.
+// Postcondition : BST has been inorder traversed and values in nodes 
+//                 have been output to out.
+// Note          : inorder uses private auxiliary function inorderAux().
+//============================================================================    
+
+    void Inorder(void){
+        
+    }
+
+//============================================================================
+// Purpose       : Graphic output of BST.
+// Precondition  : ostream out is open.
+// Postcondition : Graphical representation of BST has been output to out.
+// Note          : inorder uses private auxiliary function graphAux().
+//============================================================================       
+    void Graph(void){
+
+    }
+    
+
+
+
+    private:
+  //============================================================================
+  // Purpose       : Locate a node containing item and its parent.
+  // Precondition  : None.
+  // Postcondition : locptr points to node containing item or is
+  //                 null if not found, and parent points to its parent
+  //============================================================================
+
+    void search2(int &item, bool &found, Node *locptr, Node *parent){
+        locptr = Root;
+        parent = 0;
+        found = 0;
+
+        while(!found && locptr != 0){
+            if(item < locptr->Data){
+                parent = locptr;
+                locptr = locptr->Left;
+            }
+            else if(item > locptr->Data){
+                parent = locptr;
+                locptr = locptr->Right;
+            }
+            else{
+                found = true;
+            }
+        }
+
+    }
+
+    //============================================================================
+    // Purpose       : Inorder traversal auxiliary function.
+    // Precondition  : ostream out is open; subtreePtr points to a subtree of 
+    //                 this BST.
+    // Postcondition : Subtree of this BST with subtreePtr as root has been 
+    //                 output to out.
+    //============================================================================
+
+    void inorderAux(Node subtreePtr){
+
+    }
+    
+    //============================================================================
+    // Purpose       : Graph auxiliary function.
+    // Precondition  : ostream out is open; subtreePtr points to a subtree 
+    //                 of this BST.
+    // Postcondition : Graphical representation of subtree of this 
+    //                 BST with subtreePtr as root has been output to out, 
+    //                 indented indent spaces.
+    //============================================================================
+
+    void graphAux(int indent, Node subtreeRoot){
+
+    }
 };
 
 int main(void){
